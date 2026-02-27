0dcb3525c49888aa5b45028a047c45ce1060d961
Iaroslav Iadrov <yaroslav.yadrov@gmail.com>
2026-02-27 17:51:57 +0100

    Ctl and other changes

--- keyboards/jian/keymaps/yrslv/keymap.c
+++ keyboards/jian/keymaps/yrslv/keymap.c
@@ -52,7 +52,7 @@ void layer_switch_reset(qk_tap_dance_state_t *state, void *user_data);
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT(
-  KC_ESC, TD(ALT_BACK),   TD(Q_FORW),    KC_W,    KC_E,    KC_R,  KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC,         LAG_T(KC_RBRC),
+  KC_ESC, TD(ALT_BACK),   TD(Q_FORW),    KC_W,    KC_E,    KC_R,  KC_T,          KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,            KC_LBRC,         KC_RBRC,
           LSFT_T(KC_TAB), KC_A,          KC_S,    KC_D,    KC_F,  KC_G,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,         KC_QUOT,
           TD(CTL_SPACE),  KC_Z,          KC_X,    KC_C,    KC_V,  KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RGUI_T(KC_BSLS),
                          TD(ENT_SPACE), LT(1,KC_CAPS), TD(CMD_SHIFT),         RSFT_T(KC_ENT), LT(2,KC_SPC),  LT(1,KC_BSPC)
@@ -60,19 +60,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [1] = LAYOUT(
   KC_TRNS, KC_CAPS, KC_PSLS, KC_7, KC_8, KC_9,  KC_MINS,        KC_AMPR,   KC_COLN,   KC_PMNS,   KC_GT,   KC_NO,   KC_NO,  RSG(KC_4),
            KC_TRNS, KC_PAST, KC_4, KC_5, KC_6,  KC_EQL,         KC_EXLM,   KC_PEQL,   KC_LPRN,   KC_RPRN, KC_QUES, KC_NO,
-           KC_TRNS, KC_0,    KC_1, KC_2, KC_3,  KC_PDOT,        KC_PIPE,   KC_UNDS,   KC_LCBR,   KC_RCBR, KC_RALT,   KC_RCMD,
+           KC_LCMD, KC_0,    KC_1, KC_2, KC_3,  KC_PDOT,        KC_PIPE,   KC_UNDS,   KC_LCBR,   KC_RCBR, KC_RALT,   KC_RCTL,
                         LT(3,KC_ENT), KC_TRNS,  KC_TRNS,        KC_TRNS,   LT(3, KC_SPC),   KC_TRNS
 ),
 [2] = LAYOUT(
-  KC_TRNS, KC_CAPS,  KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_KB_VOLUME_UP,   KC_ENT,        RALT(KC_ENT), KC_RGUI, KC_RSFT, KC_PGUP,  RSG(KC_4),
+  KC_TRNS, KC_CAPS,  KC_NO,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        KC_KB_VOLUME_UP,   KC_ENT,        RALT(KC_ENT), LAG(KC_L), KC_RSFT, KC_PGUP,  RSG(KC_4),
            KC_TRNS,  KC_NO,   KC_F4,   KC_F5,   KC_F6,   KC_F11,        KC_HOME,           KC_LEFT,       KC_UP,        KC_RGHT, KC_END,  KC_PGDN,
-           KC_LCMD,  KC_LGUI, KC_F1,   KC_F2,   KC_F3,   KC_F12,        KC_KB_VOLUME_DOWN, KC_RALT,       KC_DOWN,      TD(ALT_SHIFT), KC_RSFT, KC_RCMD,
+           KC_LCMD,  KC_LGUI, KC_F1,   KC_F2,   KC_F3,   KC_F12,        KC_KB_VOLUME_DOWN, KC_RALT,       KC_DOWN,      TD(ALT_SHIFT), KC_RSFT, KC_RCTL,
                         KC_TRNS, LT(3,KC_CAPS), TD(CMD_SHIFT),        KC_TRNS, KC_TRNS,  LT(3,KC_BSPC)
 ),
 [3] = LAYOUT_symmetric(
-  RESET,   DEBUG,   KC_ASUP, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
-           XXXXXXX, KC_ASRP, KC_ASTG,  XXXXXXX, XXXXXXX, XXXXXXX,
-           EEP_RST, KC_ASDN, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+  RESET,   DEBUG,   XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+           XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
+           EEP_RST, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX,
                                        _______, XXXXXXX, _______
 )
 };
